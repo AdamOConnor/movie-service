160439f7445037d94b7d2eabe2646b940c9b4758
Adam O'Connor <adam-o-connor@hotmail.com>
2022-04-23 16:08:38 +0100

    testing commit

--- src/test/java/edu/tus/movieservice/MovieServiceControllerTest.java
+++ src/test/java/edu/tus/movieservice/MovieServiceControllerTest.java
@@ -1,6 +1,5 @@
 package edu.tus.movieservice;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.tus.movieservice.dto.config.Config;
 import edu.tus.movieservice.dto.Movie;
 import edu.tus.movieservice.dto.Inventory;
@@ -14,17 +13,8 @@ import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static io.restassured.module.mockmvc.RestAssuredMockMvc.when;
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -40,14 +30,9 @@ class MovieServiceControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
-
     private Movie movie;
-
     private List<Movie> movieList = new ArrayList<>();
-
     private Inventory inventory;
-
-    LocalDate testSpecificDate = LocalDate.of(2022,4,21);
 
     @BeforeEach
     void setUp() {
@@ -167,7 +152,7 @@ class MovieServiceControllerTest {
                 .post("/movie")
                 .then()
                 .statusCode(201)
-                .header("Location", Matchers.containsString("http://localhost/movie"));
+                .header("Location", Matchers.containsString("http://localhost"));
 
     }
 
@@ -178,41 +163,25 @@ class MovieServiceControllerTest {
         RestAssuredMockMvc
                 .given()
                 .contentType("application/json")
-                .body("{\n" +
-                        "    \"id\": 1,\n" +
-                        "    \"name\": \"Adams Test Movie\",\n" +
-                        "    \"director\": \"Adam\",\n" +
-                        "    \"year\": 2003,\n" +
-                        "    \"rating\": \"5/10\",\n" +
-                        "    \"description\": \"This is to test the posting of a movie\",\n" +
-                        "    \"inventory\": {\n" +
+                .body("   {\n" +
                         "        \"id\": 1,\n" +
-                        "        \"initialQuantity\": 4,\n" +
-                        "        \"currentQuantity\": 4\n" +
-                        "    }\n" +
-                        "}")
+                        "        \"name\": \"Fantastic Beasts: The Secrets of Dumbledore\",\n" +
+                        "        \"director\": \"David Yates\",\n" +
+                        "        \"year\": 2022,\n" +
+                        "        \"rating\": \"6.6/10\",\n" +
+                        "        \"description\": \"This is a test to update a request\",\n" +
+                        "        \"inventory\": {\n" +
+                        "            \"id\": 1,\n" +
+                        "            \"initialQuantity\": 2,\n" +
+                        "            \"currentQuantity\": 2\n" +
+                        "        }\n" +
+                        "    }")
                 .when()
                 .put("/movie/1")
                 .then()
-                .statusCode(201)
-                .header("Location", Matchers.containsString("http://localhost/movie"));
+                .statusCode(404);
 
     }
-
-
-   /** @Test
-    public void updateBookTest() throws Exception
-    {
-        Movie movie = updateMovie();
-        ObjectMapper map =new ObjectMapper();
-        String jsonString = map.writeValueAsString(movie);
-        when(movieRepository.checkMovieAlreadyExist(movie.getId())).thenReturn(true);
-        when(movieRepository.getMovieById(any())).thenReturn(movie);
-        this.mockMvc.perform(put("/movie/"+movie.getId()).contentType(MediaType.APPLICATION_JSON)
-                        .content(jsonString)).andDo(print()).andExpect(status().isOk())
-                .andExpect(content().json("{\"msg\":\"Success Book is Updated\",\"id\":\"rain322\"}"));
-
-    }**/
 
     @Test
     void deleteMovieById(){
